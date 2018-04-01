be60021a7c0d809bed435ce861433fdee3743617
clorophila <clorofila@gmail.com>
2018-04-01 20:00:47 -0300

    Resolvido problema 1190

---
+++ Iniciante/1190.py
@@ -0,0 +1,20 @@
+T = 12
+op = input()
+
+mat = []
+soma = 0
+elementos = 0
+
+for l in range(T):
+    aux = []
+    for c in range(T):
+        aux.append(float(input()))
+        if (c>l) and (c>(T-l-1)):
+            soma+=aux[c]
+            elementos+=1
+    mat.append(aux)
+
+if(op=='S'):
+    print('{:.1f}'.format(soma))
+else:
+    print('{:.1f}'.format(soma/(elementos)))
