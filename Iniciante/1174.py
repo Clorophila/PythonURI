b331992a8e29a3bc48841a1aec5e86ad64aff0af
clorophila <clorofila@gmail.com>
2018-04-21 18:46:42 -0300

    Ajustes problema 1174

--- Iniciante/1174.py
+++ Iniciante/1174.py
@@ -1,6 +1,6 @@
 numeros = []
-for case in range(10):
+for case in range(100):
     numeros.append(float(input()))
-for i in range(10):
+for i in range(100):
     if(numeros[i]<=10):
         print('A[{}] = {:.1f}'.format(i,numeros[i]))
