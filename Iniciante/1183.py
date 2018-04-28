28d2aebe10b09c8e75fafbf8b98677126a1f51d8
clorophila <clorofila@gmail.com>
2018-04-28 15:39:24 -0300

    Resolvido problema 1183

---
+++ Iniciante/1183.py
@@ -0,0 +1,18 @@
+T = 12
+op = input()
+
+mat = []
+soma = 0
+
+for l in range(T):
+    aux = []
+    for c in range(T):
+        aux.append(float(input()))
+        if(c>l):
+            soma+=aux[c]
+    mat.append(aux)
+
+if(op=='S'):
+    print('{:.1f}'.format(soma))
+else:
+    print('{:.1f}'.format(soma/((T*(T-1))/2)))
